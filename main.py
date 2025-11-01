1ce53c99318f55a468f45ea2f406bbe5677651c9
devanshmishraa <devansh21379@gmail.com>
2025-11-01 22:23:00 +0530

    created a schema using pydantic

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
+
+class Post(BaseModel):
+	title: str
+	content: str
+	published: bool = True
+	rating: Optional[int] = None
 
 @app.get("/login") #this decorator turn this into an actual path operaion. / is the root path 
 def get_user():
@@ -14,6 +22,7 @@ def get_posts():
 	return{"data": "this is the post"}
 
 @app.post("/createposts")
-def createposts(payload: dict = Body(...)):
-	print(payload)
-	return {"new post": f"title: {payload['title']}, content: {payload['content']}"}
+def createposts(post: Post):
+	print(post)
+	print(post.dict())
+	return {"data": post}
